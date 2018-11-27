40b088843685162bd1f7fb85eb063f25c9e9e72a
CMonkey <42.codemonkey@gmail.com>
2018-11-27 15:16:42 +0800

    add lombok annotation to Customer

--- src/main/java/com/example/rest/customerservice/domain/Customer.java
+++ src/main/java/com/example/rest/customerservice/domain/Customer.java
@@ -1,11 +1,16 @@
 package com.example.rest.customerservice.domain;
 
+import lombok.Data;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @Table(name = "customer")
 @Entity
+@Data
+@ToString
 public class Customer implements Serializable {
 
     @Id
